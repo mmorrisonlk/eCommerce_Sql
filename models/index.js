66eaf0788cc0a7105cbc8fa7254992e903adade2
mmorrisonlk <mmorrisonlk@gmail.com>
2021-07-06 19:01:52 -0500

    Set up basic relations between data

--- models/index.js
+++ models/index.js
@@ -6,11 +6,42 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  // through: {
+  //   model: category_id,
+  //   unique: false
+  // },
+  as: 'productCategory'
+});
+
 // Categories have many Products
+
+Category.belongsToMany(Product, {
+  // through: {
+  //   model: category_id,
+  //   unique: false
+  // },
+  as: 'categoryName'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'productDescription'
+});
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'productDescriptors'
+});
 
 module.exports = {
   Product,
